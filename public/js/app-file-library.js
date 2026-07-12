3eebdfe45b8df9e3c56ea196b3b12f62ef1e5baa
atpx4869 <atpx4869@users.noreply.github.com>
2026-07-12 23:06:08 +0800

    feat: align file library rows on desktop

--- public/js/app-file-library.js
+++ public/js/app-file-library.js
@@ -327,7 +327,7 @@ function renderFileLibrary() {
         <span class="local-col-actions">${previewBtn}${delBtn}</span>
       </div>
       <span class="local-meta-row">
-        <span class="local-col-name" title="${escapeHtml(f.fileName)}">${escapeHtml(nameDisplay)}</span>
+        <span class="local-col-name" title="${escapeHtml(nameDisplay)}">${escapeHtml(nameDisplay)}</span>
         <span class="local-col-size">${escapeHtml(formatSize(f.size))}</span>
         <span class="local-col-time">${escapeHtml(utcToBeijing(f.indexedAt || f.mtime))}</span>
         <span class="local-col-src"><span class="local-source-chip">${escapeHtml(f.source || (isLib ? '本地' : '导出'))}</span></span>
